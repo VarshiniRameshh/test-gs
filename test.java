3e570d210c3a381d2451ed91db8a16fbf8f4effb
Varshini Ramesh <71077227+VarshiniRameshh@users.noreply.github.com>
2025-05-19 21:50:44 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,8 +1,3 @@
-Risk: Inadequate Cluster Sizing for Production Workloads (Low Severity)
-Risk: Lack of Resource Inventory in ME Privacy Inventory (Medium Severity)
-Risk: Inconsistent IAM Role Creation for Multi-Region Deployments
-Exposure of Data Plane Traffic via Public Endpoints (Lack of monitoring or enforcement to ensure all traffic uses private endpoints)
-Lack of automated port management for multi-region setups.
-Lack of automated key rotation processes or oversight by the Application Team.
-Incomplete logging configurations or lack of integration with downstream systems like PAR and Surveillance.
-Incomplete monitoring setup or lack of integration with GS monitoring tools.
+Thanks for your understanding.
+
+I’m currently working on assessing risks related to MongoDB, and I'm also exploring and understanding SkyTransit, SkyFoundry, and AWS CloudFormation in more detail.
