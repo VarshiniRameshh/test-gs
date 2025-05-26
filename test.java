2821ab114b4c963ef732becf389da7b4948ad91a
Varshini Ramesh <71077227+VarshiniRameshh@users.noreply.github.com>
2025-05-26 17:26:16 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,10 +1,7 @@
 Hi Kishor,
 
-I hope you're doing well. I’m Varshini, currently working on the MongoAtlas task as part of our ongoing initiative. I was advised to connect with you to finalize the pattern for this.
+Hope you're doing well. I’m Varshini from Manjari’s team (Tech Risk). I’ve been asked to focus on MongoAtlas and was informed to connect with you to finalize the pattern for this.
 
-Would it be possible to schedule a quick call sometime next week at your convenience? Please let me know a suitable time that works for you.
+Would you be available for a quick call next week to discuss this? Please let me know a time that works best for you.
 
 Looking forward to your response.
-
-Best regards,
-Varshini
