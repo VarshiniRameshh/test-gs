8262fe1a5ddd65612909cfbed89dc6bc4b2aa8b5
Varshini Ramesh <71077227+VarshiniRameshh@users.noreply.github.com>
2025-06-20 13:47:21 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,1 +1,4 @@
-In the last call, we discussed MongoDB. For this call, the agenda remains the same, but we have a few more questions to clarify.
+Could you please let us know who is responsible for overseeing API key rotation and access control? Also, are there any plans to use CFT instead of Terraform for MongoDB-native setups moving forward?
+Could you clarify if middleware is responsible for creating projects or just organizations? Also, who typically manages the API keys—ME or application teams?
+Is the break-glass account integrated with IdP? How is PrivateLink enforcement verified?
+How are network policies enforced to block non-GS access? / What stricter controls are planned for unauthorized cluster creation?
