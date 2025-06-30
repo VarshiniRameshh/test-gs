fce7191d55b132817eb4b714883b245d03b3653c
Varshini Ramesh <71077227+VarshiniRameshh@users.noreply.github.com>
2025-06-30 13:51:22 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,7 +1,3 @@
-MongoDB Atlas uses API keys (a pair of public and private keys) to securely connect and manage resources through automation tools like Terraform and other approved DevOps systems.
+Access in MongoDB Atlas is managed using Role-Based Access Control (RBAC). Roles like DID owners, the Middleware Engineering (ME) team, and app teams are assigned specific permissions.
 
-Each API key is tied to a specific project, so it can’t be used across multiple projects — this helps keep access tightly controlled.
-
-These keys are created and managed by the Middleware Engineering (ME) team, securely stored in AWS Secrets Manager, and are automatically rotated on a regular basis to keep them safe.
-
-Access to these keys is restricted using strict allowlists and access policies, so only authorized teams or systems can use them.
+App teams use API keys to perform actions within their projects, based on the roles assigned.
