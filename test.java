8441f4be3feaf8f592c6af14512214f29981db5b
Varshini Ramesh <71077227+VarshiniRameshh@users.noreply.github.com>
2025-07-03 14:05:53 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,25 +1,5 @@
-Connectivity Review
-Tech Risk reviews and approves the connectivity requests after multiple checks including validation of technical details like source/destination, ports, environments, data classification.
-Firewall rules are edited to allow network traffic only after Connectivity Review by Tech Risk.
+Network connectivity from all 102 to both prod and non-prod MongoDB Atlas clusters is already open via approved PrivateLink endpoints.
 
-Triggered whenever a system needs to access a particular MongoDB Atlas Account
-For Prod MongoDB Atlas Account, design review approval is a pre-requisite for connectivity approval
-This serves as the actual environment poke, where connectivity is either granted or blocked
+Network connectivity from M3L non-prod to non-prod Atlas clusters and from M3L prod to prod Atlas clusters is open.
 
-Following controls need to be self-attested by application teams or validated by Tech Risk during the Tech Risk connectivity review:
-1. Secure Ports
-
-Only Port 443 is allowed for data transfer and Port 80 for OSCP certifications checks allowed. (Already enforced for CFT connectivity to SF).
-
-2. Cross-Environment Check
-
-No cross-environment access (e.g., non-prod → prod). i.e., environment classification of source/deployment should match with the MongoDB Atlas Account environment. In order to get the MongoDB Atlas Account associated with destination IP addresses in the firewall allow rule, refer to ME Portal.
-Do not allow prod to where you need access from a non-prod system to sensitive DP20+ data for testing purposes and exception is needed. Please see Non-prod to prod/sensitive data access section.
-
-3. Data Classification Check
-
-No cross-classification access
-For example:
-
-DP 30 to DP-10 (writes) is not allowed.
-DP-10 reading from DP 30 (vs) allowed unless justified and inline with design reviewed.
+Cross-environment connectivity (e.g., non-prod M3L to prod Atlas) is not allowed without case-by-case approval.
